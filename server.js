783e20ee5918f20004a04031b21495f03ca98e27
George Saussy <george.saussy@yale.edu>
2017-05-19 00:18:13 +0000

    Debug server and edit index

--- server.js
+++ server.js
@@ -8,16 +8,54 @@ var fs = require('fs');
 function calculateResponse(url, response) {
     var filename=url;
     if(url=="/") {
-        filename="index.html"
+      filename="index.html"
+    }
+    else if(url.substring(0,1)=="/") {
+      filename=url.substring(1);
     }
     var filecontents="";
     try {
-        filecontents=fs.readFileSync(filename,'utf8');
+        if(filename.substring(filename.length-5)==".html") {
+          filecontents=fs.readFileSync(filename,'utf8');
+          response.setHeader('Content-Type', 'text/html');
+          response.statusCode = 200;
+          response.end(filecontents);
+        }
+        else if(filename.substring(filename.length-3)==".js") {
+          filecontents=fs.readFileSync(filename,'utf8');
+          response.setHeader('Content-Type', 'text/javascript');
+          response.statusCode = 200;
+          response.end(filecontents);
+        }
+        else if(filename.substring(filename.length-4)==".css") {
+          filecontents=fs.readFileSync(filename,'utf8');
+          response.setHeader('Content-Type', 'text/css');
+          response.statusCode = 200;
+          response.end(filecontents);
+        }
+        else if(filename.substring(filename.length-4)==".jpg") {
+          filecontents=fs.readFileSync(filename);
+          response.setHeader('Content-Type', 'image/jpg');
+          response.statusCode = 200;
+          response.end(filecontents, "binary");
+        }
+        else {
+          filecontents="404 Error";
+          console.log("404 Error");
+          response.setHeader('Content-Type', 'text/plain');
+          response.statusCode = 404;
+          response.end(filecontents);
+        }
+          
     }
     catch(err) {
-        filecontents="404 Error"
+        filecontents="404 Error";
+        console.log("404 Error");
+        response.setHeader('Content-Type', 'text/plain');
+        response.statusCode = 404;
+        response.end(filecontents);
     }
-    response.end(filecontents);
+    
 }
 
 
